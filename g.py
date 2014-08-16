3ea58ea11d6160095dfca67ebb8d6d6ce89f5eeb
MestreLion <linux@rodrigosilva.com>
2014-08-16 05:21:59 -0300

    add global constants

---
+++ g.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2014 Rodrigo Silva (MestreLion) <linux@rodrigosilva.com>
+#    This file is part of Pylitaire game
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. See <http://www.gnu.org/licenses/gpl.html>
+#
+# Global constants and common functions
+
+import os.path
+import xdg.BaseDirectory
+
+''' As a convention, UPPERCASE indicates a constant value, and lowercase is
+    for values that may change, either via config file, command-line arguments
+    or in-game settings
+'''
+
+# General
+VERSION = "0.1"
+GAMENAME = 'pylitaire'
+
+# Paths
+GAMEDIR = os.path.abspath(os.path.dirname(__file__) or '.')
+CONFIGDIR = xdg.BaseDirectory.save_config_path(GAMENAME)
+
+# Graphics
+FPS = 30
+fullscreen = False
+screen_size = (1280, 720)
